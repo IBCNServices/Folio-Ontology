f41202ed9593beea29f4921f318a057c3b4ac74d
Bram Steenwinckel <bram.steenwinckel@ugent.be>
2019-01-21 13:09:14 +0100

    update folio ontology

--- ontology.py
+++ ontology.py
@@ -1,18 +1,3 @@
-# Copyright 2018 Bram Steenwinckel
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 import util
 output_file = 'Folio'
 o = util.Ontology(output_file)
@@ -20,10 +5,11 @@ o.add_import(util.Import('http://www.w3.org/ns/ssn/'))
 
 
 classes = ['FailureCause', 'FailureEffect',
-            'LocalEffect', 'IntermediateEffect', 'FailureMode', 'Faulty',
-            'Effect', 'Cause', 'AnomalyKnowledge',
+            'LocalEffect', 'IntermediateEffect', 'FailureMode',
+            'Effect', 'Cause', 'ConfirmedAnomaly',
             'DetectionMethod', 'Category',
-            'ConsequenceDomain', 'Criticality', 'ObservationValue']
+            'ConsequenceDomain', 'Criticality', 'Anomaly', 'fromObservation',
+            'toObservation', 'observedProperty' ]
 
 an = util.DefinitionAnnotation('FailureCause', 'Failure causes are defects in design, process, quality, or part application, which are the underlying cause of a failure or which initiate a process which leads to failure. Also, the failure effect at the highest indenture level or total system. A failure mode may have more causes.', 'en')
 o.add_annotation(an)
@@ -35,25 +21,26 @@ an = util.DefinitionAnnotation('IntermediateEffect', 'The failure effect as it a
 o.add_annotation(an)
 an = util.DefinitionAnnotation('FailureMode', 'The specific manner or way by which a failure occurs in terms of failure of the item (being a part or (sub) system) function under investigation; it may generally describe the way the failure occurs. It shall at least clearly describe a (end) failure state of the item (or function in case of a Functional FMEA) under consideration. It is the result of the failure mechanism (cause of the failure mode)..', 'en')
 o.add_annotation(an)
-an = util.DefinitionAnnotation('Faulty', 'The loss of a function under stated conditions.', 'en')
-o.add_annotation(an)
 an = util.DefinitionAnnotation('Effect', 'The effect describes one aspect of the observed phenomenon.', 'en')
 o.add_annotation(an)
-an = util.DefinitionAnnotation('Cause', 'The cause refers to the description suspected to be the source of the anomaly', 'en')
+an = util.DefinitionAnnotation('Cause', 'The cause refers to the description suspected to be the source of the ConfirmedAnomaly', 'en')
 o.add_annotation(an)
-an = util.DefinitionAnnotation('AnomalyKnowledge', 'Anomaly’s description. Should then express, at least, which part of the system is affected, by what. Ideally, this should be completed with additional information, such as which components observe the anomaly, or the worth of its management.', 'en')
+an = util.DefinitionAnnotation('ConfirmedAnomaly', 'ConfirmedAnomaly’s description. Should then express, at least, which part of the system is affected, by what. Ideally, this should be completed with additional information, such as which components observe the ConfirmedAnomaly, or the worth of its management.', 'en')
 o.add_annotation(an)
 an = util.DefinitionAnnotation('DetectionMethod', 'This relies on components’ survey to trigger a warning about abnormalities.', 'en')
 o.add_annotation(an)
-an = util.DefinitionAnnotation('Category', 'An anomaly can happen once (transient), periodically (recurrent) or definitively (permanent).', 'en')
+an = util.DefinitionAnnotation('Category', 'An ConfirmedAnomaly can happen once (transient), periodically (recurrent) or definitively (permanent).', 'en')
 o.add_annotation(an)
-an = util.DefinitionAnnotation('ConsequenceDomain', 'Each domain relates to a specific kind of consequence of the anomaly.', 'en')
+an = util.DefinitionAnnotation('ConsequenceDomain', 'Each domain relates to a specific kind of consequence of the ConfirmedAnomaly.', 'en')
 o.add_annotation(an)
-an = util.DefinitionAnnotation('Criticality', 'It is a time dependent worth attributed to the anomaly management.', 'en')
-o.add_annotation(an)
-an = util.DefinitionAnnotation('ObservationValue', 'Value describing the observation', 'en')
+an = util.DefinitionAnnotation('Criticality', 'It is a time dependent worth attributed to the ConfirmedAnomaly management.', 'en')
 o.add_annotation(an)
 
+
+an = util.DefinitionAnnotation('Anomaly', 'Description of possible unwanted behaviour', 'en')
+o.add_annotation(an)
+an = util.DefinitionAnnotation('fromObservation', 'First observation describing the underfined behaviour', 'en')
+o.add_annotation(an)
 
 
 
@@ -63,13 +50,13 @@ an = util.ExampleAnnotation('FailureEffect', 'Increased range of input data.', '
 o.add_annotation(an)
 an = util.ExampleAnnotation('FailureMode', ' Too high values.', 'en')
 o.add_annotation(an)
-an = util.ExampleAnnotation('AnomalyKnowledge', 'Anomaly’s description. Should then express, at least, which part of the system is affected, by what. Ideally, this should be completed with additional information, such as which components observe the anomaly, or the worth of its management.', 'en')
+an = util.ExampleAnnotation('ConfirmedAnomaly', 'ConfirmedAnomaly’s description. Should then express, at least, which part of the system is affected, by what. Ideally, this should be completed with additional information, such as which components observe the ConfirmedAnomaly, or the worth of its management.', 'en')
 o.add_annotation(an)
 an = util.ExampleAnnotation('DetectionMethod', 'value > x.', 'en')
 o.add_annotation(an)
 an = util.ExampleAnnotation('ConsequenceDomain', 'Malfunctioning of device or dangerous situation for end user.', 'en')
 o.add_annotation(an)
-an = util.ExampleAnnotation('Criticality', 'non-critic anomaly', 'en')
+an = util.ExampleAnnotation('Criticality', 'non-critic ConfirmedAnomaly', 'en')
 o.add_annotation(an)
 
 for c in classes:
@@ -79,7 +66,7 @@ for c in classes:
 
 properties = ['hasCause', 'hasEffect', 'hasFailureMode', 'hasLowerFailureEffect', 'hasNextFailureEffect'
             , 'isEndEffect', 'isIntermediateEffect', 'isLocalEffect',
-            'happenedAt', 'hasDetection', 'observedBy']
+            'happenedAt', 'hasDetection', 'observedBy', 'hasCategory', 'hasCriticality', 'hasDomain']
 
 an = util.DefinitionAnnotation('hasCause', 'Relation between a failure and it corresponding cause.', 'en')
 o.add_annotation(an)
@@ -101,6 +88,12 @@ an = util.DefinitionAnnotation('happenedAt', 'Relation between the failure cause
 o.add_annotation(an)
 an = util.DefinitionAnnotation('hasDetection', 'Relation between the observed element and the detection method', 'en')
 o.add_annotation(an)
+an = util.DefinitionAnnotation('hasDomain', 'Relation between the observed element and its domain', 'en')
+o.add_annotation(an)
+an = util.DefinitionAnnotation('hasCriticality', 'Relation between the observed element and the criticality', 'en')
+o.add_annotation(an)
+an = util.DefinitionAnnotation('hasCategory', 'Relation between the observed element and its category', 'en')
+o.add_annotation(an)
 
 
 for p in properties:
@@ -115,25 +108,44 @@ o.add_subclass('FailureCause', 'FailureMode')
 o.add_subclass('FailureEffect', 'FailureMode')
 o.add_subclass('LocalEffect', 'FailureEffect')
 o.add_subclass('IntermediateEffect', 'FailureEffect')
-
-o.add_subclass('Cause', 'AnomalyKnowledge')
-o.add_subclass('Effect', 'AnomalyKnowledge')
-o.add_subclass('Category', 'AnomalyKnowledge')
-o.add_subclass('ConsequenceDomain', 'AnomalyKnowledge')
-o.add_subclass('Faulty', 'AnomalyKnowledge')
-o.add_subclass('Criticality', 'AnomalyKnowledge')
-
-o.add_subclass('ObservationValue', '<http://www.w3.org/ns/sosa/Result>')
+o.add_subclass('LocalEffect', '<http://www.w3.org/ns/sosa/Observation>')
+o.add_subclass('toObservation', '<http://www.w3.org/ns/sosa/Observation>')
+o.add_subclass('fromObservation', '<http://www.w3.org/ns/sosa/Observation>')
+o.add_subclass('observedProperty', '<http://www.w3.org/ns/sosa/ObservableProperty>')
+o.add_subclass('ConfirmedAnomaly', 'Anomaly')
 
 
-rel = util.OntoSubClassRelation(util.SomeRelation('hasEffect', 'FailureEffect'), 'FailureMode')
+
+rel = util.OntoEquivalentRelation('ConfirmedAnomaly', util.SomeRelation('hasDetection', 'DetectionMethod'))
 o.add_relation(rel)
-rel = util.OntoSubClassRelation(util.SomeRelation('hasCause', 'FailureCause'), 'FailureMode')
+rel = util.OntoEquivalentRelation('ConfirmedAnomaly', util.SomeRelation('hasEffect', 'Effect'))
+o.add_relation(rel)
+rel = util.OntoEquivalentRelation('ConfirmedAnomaly', util.SomeRelation('hasCriticality', 'Criticality'))
+o.add_relation(rel)
+rel = util.OntoEquivalentRelation('ConfirmedAnomaly', util.SomeRelation('hasDomain', 'ConsequenceDomain'))
+o.add_relation(rel)
+rel = util.OntoEquivalentRelation('ConfirmedAnomaly', util.SomeRelation('hasCause', 'Cause'))
+o.add_relation(rel)
+rel = util.OntoEquivalentRelation('ConfirmedAnomaly', util.SomeRelation('hasCategory', 'Category'))
 o.add_relation(rel)
 
-rel = util.OntoEquivalentRelation('Faulty', util.SomeRelation('hasDetection', 'DetectionMethod'))
+rel = util.OntoEquivalentRelation('FailureMode', util.SomeRelation('hasCause', 'FailureCause'))
+o.add_relation(rel)
+rel = util.OntoEquivalentRelation('FailureMode', util.SomeRelation('hasEffect', 'FailureEffect'))
 o.add_relation(rel)
 
+rel = util.OntoEquivalentRelation('Anomaly', util.SomeRelation('<http://www.w3.org/ns/ssn/wasOriginatedBy>', '<http://www.w3.org/ns/ssn/Stimulus>'))
+o.add_relation(rel)
+rel = util.OntoEquivalentRelation('Anomaly', util.SomeRelation('<http://www.w3.org/ns/ssn/wasOriginatedBy>', 'fromObservation'))
+o.add_relation(rel)
+rel = util.OntoEquivalentRelation('Anomaly', util.SomeRelation('<http://www.w3.org/ns/ssn/wasOriginatedBy>', 'toObservation'))
+o.add_relation(rel)
+rel = util.OntoEquivalentRelation('Anomaly', util.SomeRelation('<http://www.w3.org/ns/ssn/wasOriginatedBy>', 'observedProperty'))
+o.add_relation(rel)
+rel = util.OntoEquivalentRelation('Anomaly', util.ExactlyRelation('<http://www.w3.org/ns/sosa/hasResult>', '<http://www.w3.org/ns/sosa/resultTime>', 1))
+o.add_relation(rel)
+rel = util.OntoEquivalentRelation('Anomaly', util.SomeRelation('<http://www.w3.org/ns/sosa/usedProcedure>', '<http://www.w3.org/ns/sosa/Procedure>'))
+o.add_relation(rel)
 
 # EquivalentClasses
 some_relation = util.SomeRelation('hasFailureMode', 'FailureMode')
